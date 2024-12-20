d769e7fbf760595d9d0e0ee4c25d33a10086c151
dk <dayka6601@gmail.com>
2024-12-20 16:34:41 +0100

    add props & style

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -24,73 +24,75 @@ export function Weather() {
             }
 
             const data = await response.json();
-            setWeather(data);
+            setWeather(data);   
 
         } catch (error) {
             setError(error.message)
         }
+
+        
     };
+
+    const weatherImg = {
+        clear : {
+            class: 'fa-solid fa-sun'
+        },
+        cloudy: {
+            class: 'fa-solid fa-cloud-sun'
+        },
+        rainy: {
+            class: 'fa-solid fa-cloud-rain'
+        }
+    }
+
+    if(weather) {
+        console.log(weather)
+    } else {
+        console.log('Cannot read Weather')
+    }
+
     return (
         <div>
             <div className="flex flex-col justify-center items-center">
+                <span className="p-5 bg-red-500 text-white rounded-lg m-5 shadow-lg font-bold" style={{
+                    display: error ? 'block' : 'none'
+                }}>{error}</span>
                 <h3>Choisissez une ville</h3>
                 <div className="flex items-center">
                     <input className="shadow-lg w-fit p-4 rounded-lg" type="text" placeholder="Entrez une ville" value={city} onChange={(e) => {
                         setCity(e.target.value)
                     }} />
-                    <button className="rounded-lg m-2 bg-black p-2 text-white" onClick={fetchWeather}>
+                    <button className="bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg m-2 bg-black p-2 text-white" onClick={fetchWeather}>
                         Rechercher
                     </button>
                 </div>
-                <span className="bg-red-500 text-white rounded-lg m-5 shadow-lg font-bold">{error}</span>
             </div>
 
             {weather && weather.main && (
-                <div className="p-20 flex flex-col justify-center border border-gray-300 rounded-lg shadow-sm">
+                <div className="text-white p-20 flex flex-col justify-center rounded-lg shadow-lg w-{300px} bg-gradient-to-r from-sky-500 to-indigo-500">
                     <h2 className="text-6xl font-bold mb-3">{weather.name}
                         <span className="font-normal" style={{
                             color: weather.main.temp > 25 ? 'red' : 'blue'
                         }}> {weather.main.temp}°C</span>
                     </h2>
-                    <span className="text-gray-400 my-2">Aujourd'hui</span>
-                    <div className="flex items-center justify-between">
-                        <div className="p-10 flex flex-col bg-white w-1/3 m-2 rounded-lg border border-gray-300">
-                            <h3 className="text-3xl font-bold my-5">Température : </h3>
-                            <span>Température réelle : {weather.main.temp}°C</span>
-                            <span>Température ressentie : {weather.main.feels_like}°C</span>
-                        </div>
-
-                        <div className="p-10 flex flex-col bg-white w-1/3 m-2 rounded-lg border border-gray-300">
-                            <h3 className="text-3xl font-bold my-5">Divers : </h3>
-                            <span>Temps actuel :  {weather.weather[0].description}</span>
-                            <span>Humidité : {weather.main.humidity}%</span>
-                            <span>Pression : {weather.main.pressure} Bar</span>
-                        </div>
-                        <div className="p-10 flex flex-col bg-white w-1/3 m-2 rounded-lg border border-gray-300">
-                            <h3 className="text-3xl font-bold my-5">Vent</h3>
-                            <span>Vitesse du vent : {weather.wind.speed} km/h</span>
-                            <span>Direction du vent : {weather.wind.deg} degrés </span>
-                        </div>
+                    <div className="p-10 flex flex-col justify-center items-center">
+                        <h3 className="text-4xl font-bold">
+                            <i className="m-3 fa-solid fa-wind"></i>
+                            Vent
+                        </h3>
+                        <span> {weather.wind.speed} km/h</span>
+                        <span> {weather.wind.deg}°</span>
+                    </div>
+                    <div className="p-10 flex flex-col justify-center items-center">
+                        <h3 className="text-4xl font-bold">
+                            Temps
+                            <i className={weather.weather[0].main === 'Clouds' ? weatherImg.cloudy : weatherImg.clear}></i>
+                        </h3>
+                        <span> {weather.weather[0].main} </span>
+                        <span> {weather.weather[0].description} </span>
                     </div>
                 </div>
             )}
-            <div className="flex">
-                <div className="p-20 my-10 flex flex-col justify-center border border-gray-300 rounded-lg shadow-sm w-1/3">
-                    <h2 className="text-4xl font-bold">
-                        Demain
-                    </h2>
-                </div>
-                <div className="p-20 my-10 ml-5 flex flex-col justify-center border border-gray-300 rounded-lg shadow-sm w-1/3">
-                    <h2 className="text-4xl font-bold">
-                        Dans 2 jours
-                    </h2>
-                </div>
-                <div className="p-20 my-10 ml-5 flex flex-col justify-center border border-gray-300 rounded-lg shadow-sm w-1/3">
-                    <h2 className="text-4xl font-bold">
-                        Dans 3 jours
-                    </h2>
-                </div>
-            </div>
         </div>
-    )
+    );
 }
